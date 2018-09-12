3b3578483a7a8aed83dc34d145f196c2a4fec0a4
Shaun Savage <swansavage@gmail.com>
2018-09-12 18:01:56 -0400

    added rental detail and rental listing page. added bootstrap and scss. add rexus architecture and routing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Header } from './shared/Header';
-import RentalList from './components/rental/RentalList';
-import { RentalDetail } from './components/rental/RentalDetail';
-import './App.css';
+import { Header } from 'shared/Header';
+import RentalListing from 'components/rental/rental-listing/RentalListing';
+import RentalDetail from 'components/rental/rental-detail/RentalDetail';
+import 'App.css';
 
 
 const store = require('./reducers').init();
@@ -21,7 +21,7 @@ class App extends Component {
                 <Header />
                 <div className='container'>
                 <Route exact path='/' render={ () => <Redirect to='/rentals' />} />
-                <Route exact path='/rentals' component={RentalList} />
+                <Route exact path='/rentals' component={RentalListing} />
                 <Route exact path='/rentals/:id' component={RentalDetail} />
                 </div>
             </div>
